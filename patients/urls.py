3c083b6d98585e8d800bdd96fa4bd80291a627a6
Nutri xpert pro <nutrixpertpro@gmail.com>
2025-10-19 07:42:41 -0300

    Pagina de login concluída e implementação da visualização de detalhes do paciente

--- patients/urls.py
+++ patients/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PatientListAPIView
+from .views import PatientListAPIView, patient_detail_view
 
 urlpatterns = [
     path("patients/", PatientListAPIView.as_view(), name="patient-list"),
+    path("patients/<int:patient_id>/", patient_detail_view, name="patient_detail"),
 ]
--- patients/views.py
+++ patients/views.py
@@ -1,5 +1,9 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from rest_framework import generics
 from .models import Patient
+from anamnesis.models import Anamnesis
+from evaluations.models import Evaluation
 from .serializers import PatientSerializer
 from rest_framework.permissions import IsAuthenticated
 
@@ -12,3 +16,16 @@ class PatientListAPIView(generics.ListAPIView):
     def get_queryset(self):
         # Filtra pacientes pelo usuário logado (nutricionista)
         return self.queryset.filter(user=self.request.user)
+
+@login_required
+def patient_detail_view(request, patient_id):
+    patient = get_object_or_404(Patient, id=patient_id, user=request.user)
+    anamnesis = Anamnesis.objects.filter(patient=patient).order_by('-created_at').first()
+    evaluations = Evaluation.objects.filter(patient=patient).order_by('-date')
+
+    context = {
+        'patient': patient,
+        'anamnesis': anamnesis,
+        'evaluations': evaluations,
+    }
+    return render(request, 'patients/patient_detail.html', context)
